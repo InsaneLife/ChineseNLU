059861025f2aa4243d05a45d625ce1947ed260d5
zhiyang.zzy <993001803@qq.com>
2020-09-14 23:53:12 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#encoding=utf-8
+'''
+@Time    :   2020/09/14 23:51:13
+@Author  :   zhiyang.zzy 
+@Contact :   zhiyangchou@gmail.com
+@Desc    :   
+todo: 
+- BiLstm + CRF
+- IDCNN + CRF
+- Joint bert
+- stack
+
+'''
+
+# here put the import lib
+import tensorflow as tf
+
+
+
+
