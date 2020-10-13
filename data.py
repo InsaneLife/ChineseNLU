af0962f5236dfbac7c6a9d1a2aec375cadafc3ca
zhiyang.zzy <993001803@qq.com>
2020-10-13 23:51:18 +0800

    modify data

--- data.py
+++ data.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
-#encoding=utf-8
+# encoding=utf-8
 '''
-@Time    :   2020/09/23 22:46:21
-@Author  :   zhiyang.zzy 
-@Contact :   zhiyangchou@gmail.com
-@Desc    :   
+Author: 	zhiyang.zzy 
+Date: 		2020-09-23 22:46:15
+Contact: 	zhiyangchou@gmail.com
+FilePath: /ChineseNLU/data.py
+Desc: 		catslu数据处理，作为数据输入格式。
+todo: 
+- 纯实体
+- 
 '''
-
 # here put the import lib
+import json
+
+
+def get_train_data(file_):
+    print("test")
+    with open(file_) as f:
+        data_set = json.load(f)
+    
+    pass
+
+if __name__ == "__main__":
+    data_path = "data/catslu/catslu_traindev/data/map/train.json"
+    get_train_data(data_path)
+    pass
