cc6b3e904e06ce15a56022ea8e0ad3765c5c6921
zhiyang.zzy <993001803@qq.com>
2020-10-19 00:04:07 +0800

    add joint bert

---
+++ joint_bert.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding=utf-8
+'''
+Author: 	zhiyang.zzy 
+Date: 		2020-10-18 23:57:33
+Contact: 	zhiyangchou@gmail.com
+FilePath: /ChineseNLU/joint_bert.py
+Desc: 		
+'''
+# here put the import lib
